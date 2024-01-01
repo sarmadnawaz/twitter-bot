0a6ffb5c66e566006f600c24cd32d0201615bf3c
sarmadnawaz <sarmadnawazkhan123@gmail.com>
2024-01-01 19:10:55 +0500

    init project

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ class Twitter:
             access_token_secret=os.getenv('TWITTER_ACCESS_TOKEN_SECRET'),
             wait_on_rate_limit=True
         )
+    
+    def tweet(self, message):
+        self.twitter.create_tweet(text=message)
+
+
+if __name__ == "__main__":
+    twitter = Twitter()
+    twitter.tweet("Hello World!")
